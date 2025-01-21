0b1db29822b3bfa69d2a613011f95d6ef509dd9e
Olalekan Alabi <alabilekanemmanuel@gmail.com>
2025-01-21 13:58:08 +0100

    `Refactor sign-up page to use async params for message handling`

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,11 +1,7 @@
 import { SignUpForm } from "@/components/signup-form"
 
-export default function Page({
-  searchParams,
-}: {
-  searchParams: { message: string };
-}) {
-  const message = searchParams?.message
+export default async function Page({ params }: { params: Promise<{ message: string }> }) {
+  const message = (await params)?.message
   return (
     <div className="flex min-h-svh w-full bg-muted items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
