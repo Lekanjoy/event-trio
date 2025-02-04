4a43a5f22f932f98c4b56cadf59df9fc41bf270a
Olalekan Alabi <alabilekanemmanuel@gmail.com>
2025-02-04 11:13:06 +0100

    `Update React and TypeScript types to version 18`

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -4,11 +4,11 @@ import * as React from "react"
 import { type DialogProps } from "@radix-ui/react-dialog"
 import { Command as CommandPrimitive } from "cmdk"
 import { Search } from "lucide-react"
-
 import { cn } from "@/lib/utils"
 import { Dialog, DialogContent } from "@/components/ui/dialog"
+import { forwardRef } from "react"
 
-const Command = React.forwardRef<
+const Command = forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive>
 >(({ className, ...props }, ref) => (
